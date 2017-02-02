45c0520198dcf35860ba77086ba983cf0677de74
Andrew chen <chy996633@qq.com>
2017-02-02 23:00:55 +0800

    Java复习：引用类型和基本类型传值

--- src/NewTest.java
+++ src/NewTest.java
@@ -1,12 +1,33 @@
+import chapter15.New;
+
+
 /**
- * Created by Andrew on 2016/10/28.
+ * 第一种情况是在栈中声明了net引用，这个引用指向堆中的net Test()对象，然后调用方法后，栈中产生另一个nt引用，指向堆中同一个对象，因此直接
+ * 修改nt.a，就相当于修改net引用指向的对象。
+ * 第二种情况是在栈中声明了net引用，这个引用指向堆中的net Test()对象，然后调用方法后，栈中产生另一个nt引用，指向堆中同一个对象。nt重新
+ * 指向一个new NewTest()对象，此时修改的不是net引用指向的对象
+ * Java中对象都是按引用传递的，只有基本类型是传值。
  */
+
 public class NewTest {
 
+    public String a;
 
+    public static void main(String[] args) {
+        NewTest net = new NewTest();
+        net.a = "abc";
+        net.change(net);
+        System.out.println(net.a);
+    }
 
+    void change(NewTest nt){
+        // change the property a
+//        nt.a = "c";
+        //not change
+        nt = new NewTest();
+        nt.a = "c";
 
-
+    }
 
 
 
