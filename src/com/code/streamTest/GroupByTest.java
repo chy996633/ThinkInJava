9b536d34cf54fad4495a5b473be7d0aab75b3812
Samuel Chen <samuel.chen@backstopsolutions.com>
2017-09-14 01:19:15 -0500

    answer to https://stackoverflow.com/questions/46209477/group-a-list-of-object-arrays-by-timestamp-in-java/46209562

--- src/com/code/streamTest/GroupByTest.java
+++ src/com/code/streamTest/GroupByTest.java
@@ -1,18 +1,67 @@
 package com.code.streamTest;
 
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalField;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import javax.ejb.Local;
 
 public class GroupByTest {
 
 
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8);
-        Map map = numbers.stream().collect(Collectors.groupingBy(e -> e / 2));
-        System.out.println(map);
 
+        List<Demo> list = new ArrayList<>();
+        LocalDateTime now = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+        Demo demo = new Demo(Timestamp.valueOf(now),"Beijing",66.40);
+        list.add(demo);
+
+        demo = new Demo(Timestamp.valueOf(now.plusMinutes(10)),"Beijing",60.40);
+        list.add(demo);
+
+        demo = new Demo(Timestamp.valueOf(now.plusMinutes(16)),"Beijing",80.40);
+        list.add(demo);
+
+        Map map = list.stream().collect(Collectors.groupingBy(e -> roundUp(e.timestamp.toLocalDateTime())));
+
+        System.out.println(map);
+    }
+
+
+    public static String roundUp(LocalDateTime time){
+        LocalDateTime temp;
+        int minutesSinceLastWhole15 = time.getMinute() % 15;
+        if (minutesSinceLastWhole15 >= 8) { // round up
+            temp = time.plusMinutes(15 - minutesSinceLastWhole15);
+        } else { // round down
+            temp = time.minusMinutes(minutesSinceLastWhole15);
+        }
+        return temp.toString();
+    }
+
+    static class Demo{
+        Timestamp timestamp;
+        String city;
+        Double price;
+
+        public Demo(Timestamp timestamp, String city, Double price){
+            this.timestamp = timestamp;
+            this.city = city;
+            this.price = price;
+        }
+
+        @Override
+        public String toString() {
+            return "{ timestamp:"+timestamp+" city:"+city+" price:"+price+" }";
+        }
     }
 
 }
