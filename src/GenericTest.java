051c036f1f2f43aef5b16e47d78835c26756bc8e
Samuel Chen <samuel.chen@backstopsolutions.com>
2017-09-28 21:57:14 -0500

    学习泛型擦除

---
+++ src/GenericTest.java
@@ -0,0 +1,10 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class GenericTest<T> {
+
+    private ArrayList<T> list = new ArrayList<T>();
+//    private T t = new T();
+//    private T[] tArray = new T[5];
+
+}
