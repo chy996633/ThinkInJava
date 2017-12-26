97dba37fa77a70cc957b4d0f95dbd62b8c5df07a
Samuel Chen <samuel.chen@backstopsolutions.com>
2017-12-26 01:53:08 -0600

    Use static generic method to refactor utils

--- src/generics/TupleTest.java
+++ src/generics/TupleTest.java
@@ -1,29 +1,29 @@
 package generics;
 
-import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertEquals;
 import java.math.BigDecimal;
-import org.junit.Assert;
 import org.junit.Test;
 
 public class TupleTest {
 
     @Test
     public void testTwoTuple() throws Exception {
-        TwoTuple tuple = new TwoTuple("first", "2");
-        assertEquals("( first , 2 )",tuple.toString());
+//        TwoTuple tuple = new TwoTuple("first", "2");
+        TwoTuple tuple = Tuple.tuple("first", "2");
+        assertEquals("( first , 2 )", tuple.toString());
     }
 
     @Test
-    public void testThreeTuple(){
-        ThreeTuple threeTuple = new ThreeTuple("A",2,new BigDecimal("2.333"));
+    public void testThreeTuple() {
+//        ThreeTuple threeTuple = new ThreeTuple("A", 2, new BigDecimal("2.333"));
+        ThreeTuple threeTuple = Tuple.tuple("A", 2, new BigDecimal("2.333"));
         assertEquals("( A , 2 , 2.333 )", threeTuple.toString());
     }
 
     @Test
-    public void testFourTuple(){
-        FourTuple fourTuple = new FourTuple("A",2,new BigDecimal("2.333"),true);
-        assertEquals("( A , 2 , 2.333 , true )",fourTuple.toString());
+    public void testFourTuple() {
+        FourTuple fourTuple = Tuple.tuple("A", 2, new BigDecimal("2.333"), true);
+        assertEquals("( A , 2 , 2.333 , true )", fourTuple.toString());
     }
 
     //fiveTuple
