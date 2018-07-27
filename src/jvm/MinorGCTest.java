41933e9d6902946763be060c45c3fd1ee362761a
Samuel Chen <samuel.chen@backstopsolutions.com>
2018-07-26 20:10:42 -0500

    JVM 内存清理配置 代码实例

--- src/jvm/MinorGCTest.java
+++ src/jvm/MinorGCTest.java
@@ -7,13 +7,38 @@ public class MinorGCTest {
 
     private static final int _1MB = 1024 * 1024;
 
-    public static void main(String[] args) {
+    public static void testMinorGC() {
         byte[] allocate1, allocate2, allocate3, allocate4;
         allocate1 = new byte[_1MB * 2];
         allocate2 = new byte[_1MB * 2];
         allocate3 = new byte[_1MB * 2];
         allocate4 = new byte[_1MB * 4];
+    }
 
+    /**
+     * -XX:+PrintTenuringDistribution -XX:MaxTenuringThreshold=1
+     */
+    public static void testTenuringThreshold() {
+        byte[] allocate1, allocate2, allocate3;
+        allocate1 = new byte[_1MB / 4];
+        allocate2 = new byte[_1MB * 4];
+        allocate3 = new byte[_1MB * 4];
+        allocate3 = null;
+        allocate3 = new byte[_1MB * 4];
+    }
+
+    public static void testTenuringThreshold2() {
+        byte[] allocate1, allocate2, allocate3, allocate4;
+        allocate1 = new byte[_1MB / 4];
+        allocate2 = new byte[_1MB / 3];
+        allocate3 = new byte[_1MB * 4];
+        allocate4 = new byte[_1MB * 4];
+        allocate4 = null;
+        allocate4 = new byte[_1MB * 4];
+    }
+
+    public static void main(String[] args) {
+        MinorGCTest.testTenuringThreshold2();
 
 
     }
