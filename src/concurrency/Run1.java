eb406906fe6a6a7d43676ced74dfaa856c957627
Samuel Chen <samuel.chen@backstopsolutions.com>
2018-09-20 00:55:36 -0500

    format

--- src/concurrency/Run1.java
+++ src/concurrency/Run1.java
@@ -2,17 +2,17 @@ package concurrency;
 
 public class Run1 implements Runnable {
 
-	private int i = 0;
+    private int i = 0;
 
-	@Override
-	public void run() {
+    @Override
+    public void run() {
 
-		for (int j = 0; j < 100; j++) {
-			i++;
-			if (i % 1000 == 0) {
-				System.out.println(i);
-			}
-		}
-	}
+        for (int j = 0; j < 100; j++) {
+            i++;
+            if (i % 1000 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
 
 }
