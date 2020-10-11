0e800bc1357213e2762ffa02e9ad43540b195919
chenhaiyue <chenhaiyue@xsyxsc.com>
2020-10-11 20:11:45 +0800

    test

--- src/Test.java
+++ src/Test.java
@@ -5,17 +5,28 @@ public class Test {
 
     private Object o;
 
-    Test(String investId) {
+    public void myMethod(String str) {
+        System.err.println("string");
     }
 
-    String s;
 
-    /**
-     * @paramargs add by zxx ,Dec 9, 2008
-     */
+    public void myMethod(Object obj) {
+        System.err.println("object");
+    }
+
 
     public static void main(String[] args) {
+        // 静态方法调用与对象实例无关
         ((Test) null).haha();
+
+        // NPE
+//        ((Test) null).test();
+
+        Test t = new TestExt();
+        TestExt tExt = new TestExt();
+        t.myMethod(null);
+        tExt.myMethod(null);
+
     }
 
     public static void haha() {
@@ -24,8 +35,21 @@ public class Test {
     }
 
     void test() {
-        System.out.println("s=" + s);
+        System.out.println("test");
     }
 
 
+}
+
+class TestExt extends Test {
+
+    @Override
+    public void myMethod(Object obj) {
+        System.err.println("ext object");
+    }
+
+    @Override
+    public void myMethod(String str) {
+        System.err.println("ext string");
+    }
 }
